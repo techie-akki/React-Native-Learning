2d7819fcccdccc2148db7f8359afcbfd2de37a84
techie-akki <chweet.cmpl.aki@gmail.com>
2021-11-01 18:33:56 +0530

    implemented list and ScrollView

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    {name: 'Raj', key: '1'},
+    {name: 'Akki', key: '2'},
+    {name: 'Anjan', key: '3'},
+    {name: 'Vivek', key: '4'},
+    {name: 'Gurpreet', key: '5'},
+    {name: 'Hardik', key: '6'},
+    {name: 'Vineet', key: '7'},
+    {name: 'Divesh', key: '8'},
+    {name: 'Parth', key: '9'},
+    {name: 'Maharsh', key: '10'},
+  ]);
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Helle, Newbie!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Welcome to <Text>React Native</Text> Tutorial.</Text>
-        <Text>Welcome to React Native Tutorial.</Text>
-        <Text>Welcome to React Native Tutorial.</Text>
-      </View>
+      
+      <ScrollView>
+        {/* {people.map((item)=>{
+          return(
+            <View  key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          );
+        })} refactoring below*/}
+        {people.map(item => (
+          <View  key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+          
+        ))}
+      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -22,19 +43,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:40,
+    paddingHorizontal:20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  header:{
-    backgroundColor:'pink',
+  item: {
+    marginTop:25,
     padding:20,
-  },
-  boldText:{
-    fontWeight:'bold',
-  },
-  body:{
-    margin:20,
-    backgroundColor:'yellow',
-    padding:20,
+    backgroundColor:'tomato',
+    fontSize:24,
   }
 });
