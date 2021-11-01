d7591910ae43876793027b4b41a97124699fbd84
techie-akki <chweet.cmpl.aki@gmail.com>
2021-11-01 18:45:24 +0530

    code refactored

--- App.js
+++ App.js
@@ -45,8 +45,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop:40,
     paddingHorizontal:20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   item: {
     marginTop:25,
